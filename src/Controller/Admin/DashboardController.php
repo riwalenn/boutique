cc7a8d5d11a42e812858429774dabd6957a9e66b
RIWALENN BAS <riwalenn@gmail.com>
2022-06-06 17:01:28 +0200

    [ADD] easy admin

---
+++ src/Controller/Admin/DashboardController.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
+use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DashboardController extends AbstractDashboardController
+{
+    #[Route('/admin', name: 'admin')]
+    public function index(): Response
+    {
+        //        return parent::index();
+        $adminUrlGenerator = $this->container->get(AdminUrlGenerator::class);
+        return $this->redirect($adminUrlGenerator->setController(UserCrudController::class)->generateUrl());
+    }
+
+    public function configureDashboard(): Dashboard
+    {
+        return Dashboard::new()
+            // the name visible to end users
+            ->setTitle('La Boutique')
+
+            // by default EasyAdmin displays a black square as its default favicon;
+            // use this method to display a custom favicon: the given path is passed
+            // "as is" to the Twig asset() function:
+            // <link rel="shortcut icon" href="{{ asset('...') }}">
+            ->setFaviconPath('favicon.svg')
+
+            // the domain used by default is 'messages'
+            ->setTranslationDomain('my-custom-domain')
+
+            // there's no need to define the "text direction" explicitly because
+            // its default value is inferred dynamically from the user locale
+            ->setTextDirection('ltr')
+
+            // set this option if you prefer the page content to span the entire
+            // browser width, instead of the default design which sets a max width
+            ->renderContentMaximized()
+
+            // set this option if you prefer the sidebar (which contains the main menu)
+            // to be displayed as a narrow column instead of the default expanded design
+            ->renderSidebarMinimized()
+
+            // by default, users can select between a "light" and "dark" mode for the
+            // backend interface. Call this method if you prefer to disable the "dark"
+            // mode for any reason (e.g. if your interface customizations are not ready for it)
+            ->disableDarkMode()
+
+            // by default, all backend URLs are generated as absolute URLs. If you
+            // need to generate relative URLs instead, call this method
+            ->generateRelativeUrls()
+            ;
+    }
+
+    public function configureMenuItems(): iterable
+    {
+        return [
+            MenuItem::section('Users'),
+            MenuItem::linkToCrud('Users', 'fa fa-user', User::class),
+        ];
+    }
+}
