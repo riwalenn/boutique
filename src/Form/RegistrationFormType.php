2650d34c72e723ed88480c6406bfefe5c1814452
RIWALENN BAS <riwalenn@gmail.com>
2022-06-06 11:39:36 +0200

    [ADD] translate in french

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -33,11 +33,11 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Entrez un mot de passe',
                     ]),
                     new Length([
-                        'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'min' => 8,
+                        'minMessage' => 'Votre mot de passe doit avoir au moins {{ limit }} caractères',
                         // max length allowed by Symfony for security reasons
                         'max' => 4096,
                     ]),
