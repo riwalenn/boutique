83718dd50ae27c1ae91363ed3f53e45fdefdc6c7
Riwalenn <7VdDQCwL3L&M6#q>
2022-06-07 13:53:21 +0200

    [ADD] la boutique

---
+++ src/Controller/LaBoutiqueController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class LaBoutiqueController extends AbstractController
+{
+    #[Route('/la/boutique', name: 'la_boutique')]
+    public function index(): Response
+    {
+        return $this->render('la_boutique/index.html.twig');
+    }
+}
