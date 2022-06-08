f751906f183ca574f5e1264279a10a980209fdf8
Riwalenn <7VdDQCwL3L&M6#q>
2022-06-08 13:21:21 +0200

    [ADD] column brand & shape in easyAdmin

--- src/Controller/Admin/ProductCrudController.php
+++ src/Controller/Admin/ProductCrudController.php
@@ -5,6 +5,7 @@ namespace App\Controller\Admin;
 use App\Entity\Product;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\MoneyField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\SlugField;
@@ -33,7 +34,11 @@ class ProductCrudController extends AbstractCrudController
             TextField::new('subtitle'),
             TextareaField::new('description'),
             MoneyField::new('price')->setCurrency('EUR'),
-            TextField::new('shape'),
+            ChoiceField::new('shape')
+                ->setChoices([
+                    'Neuf' => 'neuf',
+                    'D\'occasion' => 'occasion'
+                ]),
             AssociationField::new('category')
         ];
     }
