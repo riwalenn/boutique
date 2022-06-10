becc3c798c0f0d1472a59ec8f26db38a1beac13f
Riwalenn <7VdDQCwL3L&M6#q>
2022-06-10 13:58:49 +0200

    [ADD] adress CRUD

--- src/Controller/AccountAddressController.php
+++ src/Controller/AccountAddressController.php
@@ -4,6 +4,9 @@ namespace App\Controller;
 
 use App\Entity\Address;
 use App\Form\AddressType;
+use App\Repository\AddressRepository;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -11,6 +14,13 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class AccountAddressController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/compte/address', name: 'account_address')]
     public function index(): Response
     {
@@ -21,16 +31,63 @@ class AccountAddressController extends AbstractController
     public function add(Request $request): Response
     {
         $address = new Address();
+        if (!$address && $address->getUser() != $this->getUser()) {
+            $this->redirectToRoute('home');
+        }
         $form = $this->createForm(AddressType::class, $address);
-//
-//        $form->handleRequest($request);
-//
-//        if ($form->isSubmitted() && $form->isValid()) {
-//
-//        }
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+
+
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
 
         return $this->render('account/address_add.html.twig', [
             'form' => $form->createView()
         ]);
+    }
+
+    #[Route('/compte/address/edit/{id}', name: 'account_edit_address')]
+    public function edit(Request $request, AddressRepository $addressRepository, $id): Response
+    {
+        $address = $addressRepository->findOneBy(["id" => $id]);
+        if (!$address && $address->getUser() != $this->getUser()) {
+            $this->redirectToRoute('home');
+        }
+        $form = $this->createForm(AddressType::class, $address);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $address->setUser($this->getUser());
+
+
+            $this->entityManager->persist($address);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('account_address');
+        }
+
+        return $this->render('account/address_add.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    #[Route('/compte/address/remove/{id}', name: 'account_address_remove')]
+    public function remove(AddressRepository $addressRepository, $id): Response
+    {if ($address && $address->getUser() === $this->getUser()) {
+        $address = $addressRepository->findOneBy(["id" => $id]);
+
+            $this->entityManager->remove($address);
+            $this->entityManager->flush();
+        }
+
+        return $this->redirectToRoute('account_address');
     }
 }
