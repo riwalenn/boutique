0e90077e2d403973e4e8b532d43621dbc8330c00
RIWALENN BAS <riwalenn@gmail.com>
2022-06-12 18:44:39 +0200

    [MAJ] recap commande

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,27 +2,100 @@
 
 namespace App\Controller;
 
+use App\Classe\Cart;
+use App\Entity\Order;
+use App\Entity\OrderDetails;
 use App\Form\OrderType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrderController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/commande', name: 'order')]
-    public function index(): Response
+    public function index(Cart $cart): Response
     {
         $has_address = $this->getUser() && !empty($this->getUser()->getAddresses()->getValues());
 
         if (!$this->getUser()->getAddresses()->getValues()) {
             $this->redirectToRoute('account_add_address');
         }
+
         $form = $this->createForm(OrderType::class, null, [
             'user' => $this->getUser()
         ]);
+
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
-            'has_address' => $has_address
+            'has_address' => $has_address,
+            'cart' => $cart->getFull()
         ]);
+    }
+
+    #[Route('/commande/recapitulatif', name: 'order_recap', methods: ["POST"])]
+    public function add(Cart $cart, Request $request): Response
+    {
+        $has_address = $this->getUser() && !empty($this->getUser()->getAddresses()->getValues());
+
+        $form = $this->createForm(OrderType::class, null, [
+            'user' => $this->getUser()
+        ]);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $order = new Order();
+            $date = new \DateTimeImmutable();
+            $carriers = $form->get('carriers')->getData();
+            $delivery = $form->get('addresses')->getData();
+
+            $delivery_content = $delivery->getFirstname();
+            $delivery_content .= ' ' . $delivery->getLastname();
+            $delivery_content .= '<br/>' . $delivery->getPhone();
+            if ($delivery->getCompany()) {
+                $delivery_content .= '<br/>' . $delivery->getCompany();
+            }
+            $delivery_content .= '<br/>' . $delivery->getAddress();
+            $delivery_content .= '<br/>' . $delivery->getZipcode() . ' ' . $delivery->getCity();
+            $delivery_content .= '<br/>' . $delivery->getCountry();
+
+            $order->setUser($this->getUser());
+            $order->setCreatedAt($date);
+            $order->setCarrierName($carriers->getName());
+            $order->setCarrierPrice($carriers->getPrice());
+            $order->setDelivery($delivery_content);
+            $order->setIsPaid(0);
+
+            $this->entityManager->persist($order);
+
+            foreach ($cart->getFull() as $product) {
+                $orderDetails = new OrderDetails();
+                $orderDetails->setMyOrder($order);
+                $orderDetails->setProduct($product["product"]->getName());
+                $orderDetails->setQuantity($product["quantity"]);
+                $orderDetails->setPrice($product["product"]->getPrice());
+                $orderDetails->setTotal($product["product"]->getPrice() * $product["quantity"]);
+
+                $this->entityManager->persist($orderDetails);
+            }
+
+//            $this->entityManager->flush();
+
+            return $this->render('order/add.html.twig', [
+                'has_address' => $has_address,
+                'cart' => $cart->getFull(),
+                'carrier' => $carriers,
+                'delivery' => $delivery_content
+            ]);
+        }
+        return $this->redirectToRoute('cart');
     }
 }
--- src/Form/OrderType.php
+++ src/Form/OrderType.php
@@ -6,6 +6,7 @@ use App\Entity\Address;
 use App\Entity\Carrier;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -16,12 +17,12 @@ class OrderType extends AbstractType
         $user = $options['user'];
         $builder
             ->add('addresses', EntityType::class, [
-                'label' => 'Choisissez votre adresse de livraison',
+                'label' => false,
                 'required' => true,
                 'class' => Address::class,
                 'choices' => $user->getAddresses(),
                 'multiple' => false,
-                'expanded' => true
+                'expanded' => true,
             ])
             ->add('carriers', EntityType::class, [
                 'label' => 'Choisissez votre transporteur',
@@ -29,6 +30,12 @@ class OrderType extends AbstractType
                 'class' => Carrier::class,
                 'multiple' => false,
                 'expanded' => true
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'valider ma commande',
+                'attr' => [
+                    'class' => 'btn btn-success btn-block'
+                ]
             ])
         ;
     }
